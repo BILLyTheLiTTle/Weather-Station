2812faf9972e8d66aadc18cb901d210757286c65
Vasilios Tsapalos <v.tsapalos@kaizengaming.com>
2026-04-15 12:19:09 +0300

    Read temperature with interval

--- src/WeatherStation.cpp
+++ src/WeatherStation.cpp
@@ -1,3 +1,4 @@
+#include <float.h>
 #include "Thermistor.h"
 
 Thermistor therm(
@@ -7,24 +8,61 @@ Thermistor therm(
     25.0,      // nominal temp (25°C) from thermistor datasheet
     ThermistorConstants::BETA_3950   // Beta coefficient (β) of the thermistor
 );
-void printTemperature();
+void updateTemperature(uint32_t interval);
 
 void setup() {
     Serial.begin(9600);
 }
 
 void loop() {
-    printTemperature();
+    updateTemperature(10000L);
 
-    delay(10000);
+    //delay(10000);
 }
 
-void printTemperature() {
+void updateTemperature(uint32_t interval) {
+    static float maxMeasuredTemp = -FLT_MAX;
+    static float minMeasuredTemp = FLT_MAX;
+    static float currentTemp = NAN;
+
+    static uint32_t lastReadTime = 0;
+
+    uint32_t now = millis();
+
+    if (lastReadTime != 0 && (now - lastReadTime) < interval) {
+        return;
+    }
+    lastReadTime = now;
+
     Temperature temp = therm.readTemperatureC();
 
     if (temp.status == Temperature::OK) {
-        Serial.print(temp.value);
-        Serial.println("°C");
+        float roundedTemp = floorf(temp.value * 2.0f + 0.5f) / 2.0f;
+
+        // Print only if changed
+        if (isnan(currentTemp) || currentTemp != roundedTemp) {
+            currentTemp = roundedTemp;
+            Serial.print("Current temperature: ");
+            Serial.print(currentTemp);
+            Serial.println("°C");
+        }
+
+        // Max update
+        if (roundedTemp > maxMeasuredTemp) {
+            maxMeasuredTemp = roundedTemp;
+            Serial.print("Max temperature: ");
+            Serial.print(maxMeasuredTemp);
+            Serial.println("°C");
+        }
+
+        // Min update (independent!)
+        if (roundedTemp < minMeasuredTemp) {
+            minMeasuredTemp = roundedTemp;
+            Serial.print("Min temperature: ");
+            Serial.print(minMeasuredTemp);
+            Serial.println("°C");
+        }
+
     } else {
         Serial.print("Error in temperature sensor: ");
         Serial.println(Temperature::getName(temp.status));
